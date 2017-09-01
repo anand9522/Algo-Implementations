fa7e71e3021dba13f2644bfb4ea63c40c38a3f5f
anand <anand9522s@gmail.com>
2017-09-02 00:46:40 +0530

    Topological Sort

--- src/Graphs/TopologicalSorting.java
+++ src/Graphs/TopologicalSorting.java
@@ -1,7 +1,51 @@
 package Graphs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by anand on 01/09/17.
  */
 public class TopologicalSorting {
+    public enum Status {VISITED, VISITING, NEW}
+
+    public Map<Integer, Node> integerNodeMap = new HashMap<>();
+    class Node{
+         int nodeNumber=-1;
+         ArrayList<Node> nextNodes = new ArrayList<>();
+         Status status=Status.NEW;
+    }
+
+    public ArrayList<Integer> getTopologicalSorting(){
+        ArrayList<Integer> topologicalOrder = new ArrayList<>();
+        for (int node : integerNodeMap.keySet()) {
+            if (topologicalDFS(integerNodeMap.get(node),topologicalOrder)==-1){
+                return new ArrayList<>();
+            }
+        }
+        return topologicalOrder;
+    }
+
+    private int topologicalDFS(Node node, List<Integer> topologicalSort){
+        if (node.status == Status.VISITING){
+            return -1;
+        }
+        else if (node.status == Status.VISITED){
+            return 1;
+        }
+        else if (node.status == Status.NEW){
+            node.status = Status.VISITING;
+            for (Node nextNode:node.nextNodes){
+                if (topologicalDFS(node,topologicalSort)==-1){
+                    return -1;
+                }
+                topologicalSort.add(nextNode.nodeNumber);
+            }
+            node.status=Status.VISITED;
+        }
+        return 1;
+    }
+
 }
